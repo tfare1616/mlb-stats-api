ef870498fed7a908a69ce81ff0f70c232e286b10
Thomas Fare <thomasfare@thomass-mbp.mynetworksettings.com>
2024-04-05 22:38:07 -0400

    check in war

--- baseball_api/myApi/urls.py
+++ baseball_api/myApi/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('topProspects/', views.topProspects, name='topProspects'),
+    path('standings/', views.getStandings, name='standings'),
+    path('getBatting/', views.getBatting, name='batting'),
+    path('getWar/', views.getWar, name='war'),
+    path('getRoster/', views.getRoster, name='roster'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- baseball_api/myApi/views.py
+++ baseball_api/myApi/views.py
@@ -1,7 +1,9 @@
-import pandas as pd
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .pybaseballgit.pybaseball import top_prospects
+import numpy as np
+from .pybaseballgit.pybaseball import top_prospects, standings, batting_stats, playerid_lookup, bwar_bat, bwar_pitch, \
+    pitching_stats, rosters
+
 
 @api_view(['PUT'])
 def topProspects(request):
@@ -11,3 +13,76 @@ def topProspects(request):
     prospects = top_prospects(team, playerType)
     prospects = prospects.fillna(0).groupby('Rk').apply(lambda x: x.to_dict(orient='records')[0])
     return Response({'prospects': prospects})
+
+
+@api_view(['PUT'])
+def getStandings(request):
+    conv = lambda i: i or None
+    date = int(conv(request.data.get('date')))
+    currentStandings = standings(date)
+    for i, s in enumerate(currentStandings):
+        currentStandings[i]['L'] = currentStandings[i]['L'].astype(int)
+        currentStandings[i] = s.groupby('Tm').apply(
+            lambda x: x.to_dict(orient='records')[0])
+
+    return Response({'standings': currentStandings})
+
+
+@api_view(['PUT'])
+def getBatting(request):
+    p = playerid_lookup(request.data.get('lastName'), request.data.get('firstName'))
+    firstYear = p['mlb_played_first'][0]
+    lastYear = p['mlb_played_last'][0]
+    key = str(p['key_fangraphs'][0])
+    bWar = bwar_bat().fillna(0)
+    bWar = bWar.loc[bWar['player_ID'] == p['key_bbref'][0]].groupby('year_ID').apply(
+        lambda x: x.to_dict(orient='records')[0])
+    fangraphsStats = batting_stats(firstYear, lastYear, qual=10, players=key).fillna(0).groupby('Season').apply(
+        lambda x: x.to_dict(orient='records')[0])
+
+    return Response({'fangraphs': fangraphsStats, 'bwar': bWar})
+
+
+@api_view(['PUT'])
+def getWar(request):
+    p = playerid_lookup(request.data.get('lastName'), request.data.get('firstName'), fuzzy=True)
+    firstYear = p['mlb_played_first'][0]
+    lastYear = p['mlb_played_last'][0]
+    key = str(p['key_fangraphs'][0])
+    bWarHit = bwar_bat().fillna(0)
+    bWarPitch = bwar_pitch().fillna(0)
+
+    try:
+        bWarHit = bWarHit.loc[bWarHit['player_ID'] == p['key_bbref'][0]].groupby('year_ID').apply(
+            lambda x: x.to_dict(orient='records')[0])
+    except:
+        bWarPitch = []
+
+    try:
+        fangraphsHitting = batting_stats(firstYear, lastYear, qual=10, players=key).fillna(0).groupby('Season').apply(
+            lambda x: x.to_dict(orient='records')[0])
+    except:
+        fangraphsHitting = []
+
+    try:
+        bWarPitch = bWarPitch.loc[bWarPitch['player_ID'] == p['key_bbref'][0]].groupby('year_ID').apply(
+            lambda x: x.to_dict(orient='records')[0])
+    except:
+        bWarPitch = []
+
+    try:
+        fangraphsPitch = pitching_stats(firstYear, lastYear, qual=10, players=key).fillna(0).groupby('Season').apply(
+            lambda x: x.to_dict(orient='records')[0])
+    except:
+        fangraphsPitch = []
+
+    return Response({'fangraphsHitting': fangraphsHitting, 'brefHitting': bWarHit, 'fangraphsPitching': fangraphsPitch,
+                     'brefPitching': bWarPitch})
+
+
+@api_view(['PUT'])
+def getRoster(request):
+    conv = lambda i: i or None
+    date = int(conv(request.data.get('date')))
+    roster = rosters(date)
+    return Response({'roster': roster})
